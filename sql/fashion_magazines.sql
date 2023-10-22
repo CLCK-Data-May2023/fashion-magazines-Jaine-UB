6ca2df9d2f10b8dba54ec1da58059c982346ac74
Unurjargal Boldbaatar <aja20.aja10success@gmail.com>
2023-10-22 15:58:09 -0500

    updating sql code

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -3,17 +3,6 @@ FROM customers
 C JOIN (SELECT orders.customer_id,subscriptions.price_per_month*orders.subscription_length as due,
 orders.order_status
 FROM orders 
-OUTER JOIN subscriptions ON orders.subscription_id = subscriptions.subscription_id
+O JOIN subscriptions ON orders.subscription_id = subscriptions.subscription_id
 WHERE orders.order_status = "unpaid" and descriptions = "Fashion Magazine" ) unpaid ON customers.customer_id = unpaid.customer_id
-GROUP BY customers.customer_name ; 
-
-
-
-
-
-
-
-
-
-
--- Add your sql here 
+GROUP BY customers.customer_name ;
