236644bdfbabb38e38e8d8175a4cf2eaa99c824c
Unurjargal Boldbaatar <aja20.aja10success@gmail.com>
2023-10-22 13:10:01 -0500

    updating code

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,19 @@
--- Add your sql here
+SELECT c.customer_name , sum (unpaid.due)
+FROM customers
+C JOIN (SELECT orders.customer_id,subscriptions.price_per_month*orders.subscription_length as due,
+orders.order_status
+FROM orders 
+OUTER JOIN subscriptions ON orders.subscription_id = subscriptions.subscription_id
+WHERE orders.order_status = "unpaid" and descriptions = "Fashion Magazine" ) unpaid ON customers.customer_id = unpaid.customer_id
+GROUP BY customers.customer_name ; 
+
+
+
+
+
+
+
+
+
+
+-- Add your sql here 
