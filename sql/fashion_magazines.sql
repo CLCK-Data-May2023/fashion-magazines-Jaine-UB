53656ab657d8b9499ba64056ce39364d0c5557fc
Unurjargal Boldbaatar <aja20.aja10success@gmail.com>
2023-10-22 18:54:00 -0500

    trying from gitbash

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,9 @@
--- Add your sql here
+SELECT customer_name AS "Customer", sum(unpaid.due) AS "Amount Due"
+FROM customers
+C JOIN
+(SELECT orders.customer_id, subscriptions.price_per_month * subscriptions.subscription_length as due,
+orders.order_status
+FROM orders 
+INNER JOIN subscriptions ON orders.subscription_id = subscriptions.subscription_id
+WHERE orders.order_status = "unpaid" and subscriptions.description = "Fashion Magazine" ) unpaid ON c.customer_id = unpaid.customer_id
+GROUP BY customer_name ;
