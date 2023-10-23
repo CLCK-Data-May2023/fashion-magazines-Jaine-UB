e579a68c4aeca70f530267977f692701b5a53c61
Unurjargal Boldbaatar <aja20.aja10success@gmail.com>
2023-10-22 22:17:56 -0400

    final update on sql code

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,9 +1,9 @@
-SELECT customer_name AS "Customer", PRINTF("$%5.f.00", unpaid.due) AS "Amount Due"
+SELECT customer_name AS "Customer", PRINTF("$%5.f.00", sum(unpaid.totaldue)) AS "Amount Due"
 FROM customers
-C JOIN
-(SELECT orders.customer_id, subscriptions.price_per_month * subscriptions.subscription_length as due,
+C JOIN(
+SELECT orders.customer_id, subscriptions.price_per_month*subscriptions.subscription_length as totaldue,
 orders.order_status
 FROM orders 
-INNER JOIN subscriptions ON orders.subscription_id = subscriptions.subscription_id
+JOIN subscriptions ON orders.subscription_id = subscriptions.subscription_id
 WHERE orders.order_status = "unpaid" and subscriptions.description = "Fashion Magazine" ) unpaid ON c.customer_id = unpaid.customer_id
-GROUP BY customer_name ;
+GROUP BY customer_name;
